44f7443cd8d1548a69384ed80315748f37d79b19
nutchanokp <nutchanok.cc@gmail.com>
2023-08-13 20:40:49 +0700

    edit filter array

--- filter_array/src/main/java/com/nut/cdev/filter_array/ext/FilterArrayExt.kt
+++ filter_array/src/main/java/com/nut/cdev/filter_array/ext/FilterArrayExt.kt
@@ -1,7 +1,7 @@
 package com.nut.cdev.filter_array.ext
 fun main() {
-    val array1 = arrayOf(1, 2, 3, 4, 5)
-    val array2 = arrayOf(3, 4, 5, 6, 7)
+    val array1 = arrayOf(1, 2, 3, 4, 5,10,9,33,55,20)
+    val array2 = arrayOf(33,3, 4, 5,55, 6, 7)
 
     val filteredArray = filterArray(array1, array2)
 
@@ -28,5 +28,18 @@ fun filterArray(array1: Array<Int>, array2: Array<Int>): Array<Int> {
         }
     }
 
-    return filteredList.toTypedArray()
+    val filteredArray = mutableListOf<Int>()
+
+    for (item1 in array1) {
+        for (item2 in array2) {
+            if (item1 == item2) {
+                filteredArray.add(item1)
+                break
+            }
+        }
+    }
+
+    val result = filteredArray.toTypedArray()
+
+    return result
 }
